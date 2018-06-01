f7102eb031a649177ac1d9244f7a6123d2e0cf76
Ariel Rodriguez <acr012@ucsd.edu>
2018-05-31 20:00:14 -0700

    intial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express'); //access express library
+                                    //common js modules
+                                    //Node.js system to share modules
+                                    //
+                                    //note: import express from 'express';
+                                    //      uses ES2015 modules system
+                                    //      Node.js does not support natively
+                                    //      support this
+
+const app = express();              //generates app
+                                    //route handlers will be associated w/ app
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;      //heroku dynamic port binding at runtime
+                                    //does not appear in development env
+app.listen(PORT);                   //defines port
